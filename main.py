fda856e6fa07e56a8adb797abf153f6fec84b70d
Agent007little <alekc_kg@mail.ru>
2025-02-09 15:48:04 +0300

    create spacy model

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from spacy.lang.ru import Russian
+
+model = Russian()
+
+text = model("Фильм понравился, хорошие актёры, интересный сюжет, отличные спецэффекты.")
+
+text_list = [i for i in text]
