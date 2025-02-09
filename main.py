935548f848ead6b5a6bcd1a2561e65013a03efba
Agent007little <alekc_kg@mail.ru>
2025-02-09 15:54:20 +0300

    make filter words

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from spacy.lang.ru import Russian
+from spacy.lang.ru.stop_words import STOP_WORDS
 
 model = Russian()
 
 text = model("Фильм понравился, хорошие актёры, интересный сюжет, отличные спецэффекты.")
 
 text_list = [i for i in text]
+filter_stop_words = [i for i in text_list if i not in STOP_WORDS]
+print(filter_stop_words)
