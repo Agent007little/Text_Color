37936f88094ed6536ca0b9c51699af9c6bb7c777
Agent007little <alekc_kg@mail.ru>
2025-02-16 15:44:38 +0300

    change model on ru_core_news_md.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from spacy.lang.ru import Russian
 from spacy.lang.ru.stop_words import STOP_WORDS
+import ru_core_news_md
 
-model = Russian()
+model = ru_core_news_md.load()
 
-text = model("Фильм понравился, хорошие актёры, интересный сюжет, отличные спецэффекты.")
+text = model("авось Фильм понравился, хорошие актёры, интересный сюжет, отличные спецэффекты. казались")
 
-text_list = [i for i in text]
+text_list = [i.lemma_ for i in text]
+
+
 filter_stop_words = [i for i in text_list if i not in STOP_WORDS]
-print(filter_stop_words)
